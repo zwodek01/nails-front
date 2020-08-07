c87baa7866d93e78a7a2708f470fbfe019d1269e
Mateusz Zawadzki <wpzawadzki.m@gmail.com>
2020-08-07 19:34:26 +0200

    feat: services section implemented

--- components/common/components/whyMe.js
+++ components/common/components/whyMe.js
@@ -1,43 +1,31 @@
 import React from 'react';
-import SectionContentCard from '../../../common/components/sectionContentCard';
+import SectionContentCard from './sectionContentCard';
 import LazyLoad from 'react-lazyload';
 import ScrollAnimation from 'react-animate-on-scroll';
 
 const WhyMe = () => {
   return (
-    <section className="px-4 my-12 max-w-screen-xl mx-auto flex flex-col items-center justify-center lg:flex-row lg:justify-between lg:my-24">
-      <div className="flex justify-center flex-1">
-        <LazyLoad>
-          <ScrollAnimation
-            animateIn="fadeIn"
-            animateOnce
-            delay={500}
-            offset={200}
-          >
+    <ScrollAnimation animateIn="fadeIn" animateOnce delay={500} offset={200}>
+      <section className="px-4 my-12 max-w-screen-xl mx-auto flex flex-col items-center justify-center lg:flex-row lg:justify-between lg:my-24">
+        <div className="flex justify-center flex-1">
+          <LazyLoad offset={100}>
             <img
               alt="Zdjęcie paznokci"
               className=" md:block md:max-w-md lg:max-w-lg xl:max-w-2xl"
               src="/image-2.png"
             />
-          </ScrollAnimation>
-        </LazyLoad>
-      </div>
-      <div className="my-6 lg:my-0 lg:ml-4">
-        <ScrollAnimation
-          animateIn="fadeIn"
-          animateOnce
-          delay={200}
-          offset={200}
-        >
+          </LazyLoad>
+        </div>
+        <div className="my-6 lg:my-0 lg:ml-4">
           <SectionContentCard
             blackText="Mój salon ma na celu dostarczenie najlepszych doświadczeń w projektowaniu paznokci i najwyższej jakości obsługi klienta."
             grayText="Korzystam z całkowicie naturalnych, organicznych produktów do ciała, trudno dostępnych polskich marek i kolorów. Mam również najlepszą kawę w mieście."
             mainTitle
             title="Dlaczego klienci mnie wybierają?"
           />
-        </ScrollAnimation>
-      </div>
-    </section>
+        </div>
+      </section>
+    </ScrollAnimation>
   );
 };
 
